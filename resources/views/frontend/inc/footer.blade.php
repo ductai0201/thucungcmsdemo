c49fe54180155fca617d81a0fe51e05b54a00d3f
ductai0201 <ductai0201@gmail.com>
2024-03-13 16:24:19 +0700

    thucungCMS13/03

---
+++ resources/views/frontend/inc/footer.blade.php
@@ -0,0 +1,90 @@
+<div id="footer" class="bg-light pb-4">
+    <footer class="footerContainer container m-auto">
+        <div class="row">
+            <div class="col-lg-4 footerContainerLeft">
+                <h3 class="footer-left--title footer-title font-weight-bold mb-4">Chúng tôi có thể giúp bạn?</h3>
+                <p class="footer-left--item footer-title">Chúng tôi luôn hỗ trợ, trả lời các thắc mắc của bạn 24/7</p>
+                <div class="mt-4">
+                    <span class="phoneNumberFooter font-weight-bold d-block">0123.456.789</span>
+                    <span class="d-block footer-left--item footer-item">Email: <a href="mailto:support@gmail.com" class="underline footer-left--item">support@gmail.com</a></span>
+                </div>
+                <div class="mt-4">
+                    <span class="d-block footer-left--item footer-item">Thời gian làm việc</span>
+                    <span class="d-block footer-left--item footer-item">Thứ 2 - Chủ nhật 09:00 - 20:00 </span>
+                </div>
+            </div>
+            <div class="col-lg-8 pl-4">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h3 class="footer-right--title footer-title font-weight-bold mb-4">Để chúng tôi giúp bạn</h3>
+                        <ul class="footer-list-menu list-unstyled lh-180%">
+                            <li><a class="footer-right--menuItem footer-item" href="#">Đơn đặt hàng của bạn</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Trả hàng & Thay thế</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Phí vận chuyển & Chính sách</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Chính sách hoàn tiền và đổi trả</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Chính sách bảo mật</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4">
+                        <h3 class="footer-right--title footer-title font-weight-bold mb-4">Đồng hành cùng chúng tôi</h3>
+                        <ul class="footer-list-menu list-unstyled">
+                            <li><a class="footer-right--menuItem footer-item" href="#">Trở thành người bán của chúng tôi</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Trở thành Affilate</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Quảng cáo sản phẩm của bạn</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Trở thành nhà cung cấp</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Bán dịch vụ của bạn với chúng tôi</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4">
+                        <h3 class="footer-right--title footer-title font-weight-bold mb-4">Tìm hiểu về chúng tôi</h3>
+                        <ul class="footer-list-menu list-unstyled">
+                            <li><a class="footer-right--menuItem footer-item" href="#">Về chúng tôi</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Đánh giá của khách hàng</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Các địa điểm cửa hàng</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Trung tâm trợ giúp</a></li>
+                            <li><a class="footer-right--menuItem footer-item" href="#">Theo dõi thêm</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="followUs container pt-4">
+        <div class="row justify-content-between">
+            <div class="col-lg-6">
+                <div class="followUs--left d-flex align-items-center gap-2">
+                    <span class="fs-14px">Theo dõi chúng tôi: </span>
+                    <div class="items-FollowUs">
+                        <img class="w-20px" src="{{ static_asset('assets/img/icons8-facebook-30.png') }}" alt="">
+                    </div>
+                    <div class="items-FollowUs">
+                        <img class="w-20px" src="{{ static_asset('assets/img/icons8-instagram-30.png') }}" alt="">
+                    </div>
+                    <div class="items-FollowUs">
+                        <img class="w-20px" src="{{ static_asset('assets/img/icons8-telegram-32.png') }}" alt="">
+                    </div>
+                    <div class="items-FollowUs">
+                        <img class="w-20px" src="{{ static_asset('assets/img/icons8-twitterx-30.png') }}" alt="">
+                    </div>
+                    <div class="items-FollowUs">
+                        <img class="w-20px" src="{{ static_asset('assets/img/icons8-youtube-30.png') }}" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="payment--right d-flex align-items-center justify-content-end">
+                    <span class="text-14px"> Chúng tôi chấp nhận:</span>
+                    <div class="itemPayment">
+                        <img src="{{ static_asset('assets/img/visa.png') }}" alt="">
+                    </div>
+                    <div class="itemPayment">
+                        <img src="{{ static_asset('assets/img/mastercard.png') }}" alt="">
+                    </div>
+                    <div class="itemPayment">
+                        <img src="{{ static_asset('assets/img/paypal.png') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
